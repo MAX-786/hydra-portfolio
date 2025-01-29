c213a801a54c2407890a87b32d9607cfe9c1a81f
Mohammad K. Hussain <hussainmohammad328@gmail.com>
2025-01-30 03:53:02 +0530

    fix button bg in dark mode
    Signed-off-by: Mohammad K. Hussain <hussainmohammad328@gmail.com>

--- portfolio/src/components/AnimatedBackground.tsx
+++ portfolio/src/components/AnimatedBackground.tsx
@@ -5,7 +5,7 @@ import { useTheme } from "../contexts/ThemeContext"
 
 const particles = [
   "Hi",
-  "Hello",
+  "Yahallo!",
   "もしもし",
   "Bonjour",
   "Hola",
@@ -100,7 +100,7 @@ export default function AnimatedBackground() {
         ctx.save()
         ctx.translate(this.x, this.y)
         ctx.rotate(this.angle)
-        ctx.font = "12px Arial"
+        ctx.font = "16px Arial"
         ctx.fillStyle = theme === "light" ? "rgba(55, 61, 240, 0.8)" : "rgba(200, 200, 255, 0.8)"
         ctx.fillText(this.text, -ctx.measureText(this.text).width / 2, 0)
         ctx.restore()
--- portfolio/src/components/BlogButton.tsx
+++ portfolio/src/components/BlogButton.tsx
@@ -6,9 +6,9 @@ export default function BlogButton() {
       href="/blog"
       className="group relative inline-block overflow-hidden rounded-full dark:bg-blue px-8 py-3 text-white"
     >
-      <span className="absolute inset-0 bg-white transition-transform duration-300 ease-out group-hover:translate-y-full"></span>
-      <span className="relative font-semibold transition-colors duration-300 text-white group-hover:text-blue-600 dark:group-hover:text-blue-400">
-        Visit My Blog
+      <span className="absolute inset-0 bg-black transition-transform duration-300 ease-out group-hover:translate-y-full dark:bg-white"></span>
+      <span className="relative font-semibold transition-colors duration-300 text-white group-hover:text-blue-600 dark:text-black dark:group-hover:text-gray-400">
+        My Garden
       </span>
     </Link>
   )
