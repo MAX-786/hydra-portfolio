f72847960ce417e8f80e466471b9a572e81b6099
Mohammad K. Hussain <hussainmohammad328@gmail.com>
2025-02-08 07:00:50 +0530

    fix https issue

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    CMS_URL: 'https://15.207.100.48/mkhismkh/++api++',
+    CMS_URL: 'https://15.207.100.48/++api++',
   },
   async rewrites() {
     return [
