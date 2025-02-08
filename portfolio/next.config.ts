22e8699f64087e1e70b2b2956376f6e2f1187865
Mohammad K. Hussain <hussainmohammad328@gmail.com>
2025-02-08 07:26:31 +0530

    fix clean up

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    CMS_URL: 'https://15.207.100.48/++api++',
+    CMS_URL: 'https://15.207.100.48/mkhismkh/++api++',
   },
   async rewrites() {
     return [
